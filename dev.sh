1a3234bd5cc653d5f0841e48505e1ea0c83299eb
tnahs <31777797+tnahs@users.noreply.github.com>
2020-09-17 20:14:56 -0700

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
-rm -rf ~/.readstor
-rm -rf ~/ReadStor
-mkdir ~/ReadStor
+rm -rf ~/.readstordev
+rm -rf ~/ReadStorDev
+mkdir ~/ReadStorDev
 python src/readstor/__main__.py --log_level DEBUG --env DEVELOPMENT
